b09821ecad3d1133d45aab63f11c96580e01101c
alejandro.domingo.ga <mralexsito@gmail.com>
2025-10-13 17:59:25 +0200

    actualización

--- PizzeriaCosaNostra-master/src/main/java/dao/IngredientesDAO.java
+++ PizzeriaCosaNostra-master/src/main/java/dao/IngredientesDAO.java
@@ -1,4 +1,11 @@
 package dao;
 
+import model.Ingrediente;
+
+import java.util.List;
+
 public interface IngredientesDAO {
+    public List<Ingrediente> recuperarIngredientes();
+
+
 }
---
+++ PizzeriaCosaNostra-master/src/main/java/dao/IngredientesIMPL.java
@@ -0,0 +1,46 @@
+package dao;
+
+import model.Ingrediente;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientesIMPL implements IngredientesDAO {
+
+    String path="PizzeriaCosaNostra-master/src/main/resources/ingredientes.dat";
+
+    public List<Ingrediente> recuperarIngredientes(){
+        //deserializamos y retornamos una lista de ingredientes
+        try (ObjectInputStream ois= new ObjectInputStream(new FileInputStream(path))){
+            return (List<Ingrediente>) ois.readObject();
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return new ArrayList<>();
+    }
+
+    public void anadirIngrediente(Ingrediente ing){
+        List<Ingrediente> listaIngredientes = recuperarIngredientes();
+        listaIngredientes.add(ing);
+        guardarIngredientes(listaIngredientes);
+    }
+
+    //le pasamos una lista para poder saber los ingredientes que vamos a querer serializar posteriormente
+    public void guardarIngredientes(List<Ingrediente> lista){
+        //serializamos la lista de ingredientes
+        try(ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream(path))){
+            oos.writeObject(lista);
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
